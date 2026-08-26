424e69cee7c7a0beafc3927a9e02a9f383f8f2bf
Husain <hussainalmajed.2005@gmail.com>
2026-08-27 02:03:08 +0300

    Created the show function in the inspection controller

--- controllers/inspection.js
+++ controllers/inspection.js
@@ -25,7 +25,22 @@ const create = async (req , res) => {
     }
 }
 
+const show = async (req , res) => {
+    try{
+        const inspection = await Inspection.findById(req.params.inspectionId).populate('agreement')
+
+        if (!inspection) {
+            return res.status(404).json({ message: 'Inspection not found' })
+        }
+
+        res.status(200).json(inspection)
+    } catch(error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
 module.exports = {
     index,
     create,
+    show,
 }
