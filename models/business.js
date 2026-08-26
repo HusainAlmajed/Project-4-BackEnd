ecfef4b30cc02699b1701101452083bf0e824419
unknown <jonykadhem@gmail.com>
2026-08-26 10:20:32 +0300

    complited the business model schema

---
+++ models/business.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+const businessSchema = new mongoose.Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        enum: ["shop", "property", 'insurance'],
+        default: "customer",
+    },
+    customers: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        }
+    ],
+
+    employees: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        }
+    ],
+    profileImage: {
+        type: String,
+        default: "",
+    },
+
+}, { timestamps: true })
+
+
+const Business = mongoose.model('Business', businessSchema)
+module.exports = Business
