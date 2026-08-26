40a6a85424144260e93a909a9d36d9f7db913db8
Zuhair05 <alshabaq98@gmail.com>
2026-08-26 14:33:24 +0300

    add enum to the agreement type

--- models/agreement.js
+++ models/agreement.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const agreementSchema = new  mongoose.Schema({
     type: {
         type: String,
+        enum: ['warranty', 'insurance'],
         required: true,
     },
     startDate: {
