6631a56f233f69fb6506e2ad594f807e2c92d5bb
Husain <hussainalmajed.2005@gmail.com>
2026-08-27 02:00:39 +0300

    Created the create function for inspection

--- controllers/inspection.js
+++ controllers/inspection.js
@@ -10,6 +10,22 @@ const index = async (req , res) => {
     }
 }
 
+const create = async (req , res) => {
+    try {
+        const inspection = await Inspection.create({
+            inspectionType: req.body.inspectionType,
+            images: req.body.images,
+            notes: req.body.notes,
+            date: req.body.date,
+            agreement: req.body.agreement,
+        })
+        res.status(201).json(inspection)
+    } catch(error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
 module.exports = {
     index,
+    create,
 }
