57395c1eaf8300996201196c2ab43be92e313868
Zuhair05 <alshabaq98@gmail.com>
2026-08-26 15:37:32 +0300

    create func for agreement

--- controllers/agreement.js
+++ controllers/agreement.js
@@ -13,6 +13,25 @@ const index = async (req, res) => {
     }
 }
 
+const create = async (req, res) => {
+    try{
+        const agreement = await Agreement.create({
+            type: req.body.type,
+            startDate: req.body.startDate,
+            endDate: req.body.endDate,
+            status: req.body.status,
+            description: req.body.description,
+            owner: req.user._id,
+            customer: req.user._id,
+            asset: req.body.asset,
+        });
+        res.status(201).json(agreement);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
 module.exports = {
     index,
+    create,
 }
