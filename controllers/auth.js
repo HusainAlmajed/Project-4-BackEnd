46a60b89b6ad2fe4dd64eccfea700db7c5f0c3cb
unknown <jonykadhem@gmail.com>
2026-09-01 12:22:12 +0300

    made the image dose not desapear after refresh

--- controllers/auth.js
+++ controllers/auth.js
@@ -130,6 +130,25 @@ const signIn = async (req, res) => {
     }
 }
 
+const show = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userId)
+
+        if (!user) {
+            return res.status(404).json({
+                err: "User not found"
+            })
+        }
+
+        res.status(200).json(user)
+
+    } catch (error) {
+        res.status(400).json({
+            err: error.message
+        })
+    }
+}
+
 const update = async (req, res) => {
     try {
 
@@ -166,4 +185,5 @@ module.exports = {
     ownerSignUp,
     signIn,
     update,
+    show,
 }
